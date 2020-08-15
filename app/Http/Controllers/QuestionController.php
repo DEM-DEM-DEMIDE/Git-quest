0e5ef5f92a0a9035e608575a742a1cc5c8d48220
DEM-DEM-DEMIDE <kyobokudemdem@gmail.com>
2020-08-16 02:57:43 +0900

    2nd-commit

---
+++ app/Http/Controllers/QuestionController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Course;
+use Illuminate\Http\Request;
+
+class QuestionController extends Controller
+{
+
+    public function top()
+
+    {
+
+
+        return view('top');
+    }
+}
